d24fddb8a8a4f3473ef51a6b5fb7a8180188a49c
Andr3sRamBu <andresramirez224@unisangil.edu.co>
2025-03-08 18:40:52 -0500

    Update paneles.py
    Cálculos correcciones

--- paneles.py
+++ paneles.py
@@ -1,18 +1,18 @@
-"quiz 1"
+#Quizz
+
+#Entradas
 print("bienvenido  al proframa de calculo de paneles solares")
 import math
-consumoenergia = float(input("ingrese el consumo de energia en kWh: "))
-horas = float(input("ingrese el numero de horas de sol: "))
-eficiencia = float(input("ingrese la eficiencia del panel: "))
-efinciencia1 = eficiencia/100
-anchopanel = float(input("ingrese el ancho del panel: "))
-largopanel = float(input("ingrese el largo del panel: "))
-areapanel = anchopanel*largopanel
-print(f"el area del panel es: {areapanel} m^2")
-radiacionpromedio=float(input("ingrese la radiacion promedio por metro cuadrado: "))
-potenciadia=areapanel*radiacionpromedio*efinciencia1
-print(f"la potencia diaria del panel es: {potenciadia} kWh")
-potenciaño=potenciadia*365
-print(f"la potencia anual del panel es: {potenciaño} kWh")
-numpaneles=math.ceil(consumoenergia/potenciaño)
-print(f"el numero de paneles necesarios es: {numpaneles}")
+Consumo_anual = float(input("Ingrese el consumo anual de energía en KWh: "))
+Eficiencia_Panel = float(input("Ingrese la eficiencia del panel solar: "))
+Superficie_Panel = float(input("Ingrese la superficie del panel solar en m2: "))
+Horas_sol = float(input("Ingrese las horas de sol al día: "))
+Radiacion_solar = float(input("Ingrese la radiación solar en KWh/m2: "))
+
+
+#Calculos
+potencia_diaria = Superficie_Panel * Radiacion_solar * Eficiencia_Panel
+potencia_anual = potencia_diaria * 365
+Cantidad_paneles = Consumo_anual / potencia_anual
+superficie_total = Cantidad_paneles * Superficie_Panel
+
